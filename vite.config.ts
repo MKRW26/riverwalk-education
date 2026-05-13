a5718b83aea2bdc0b98d2ed94f583500e659fbd9
MKRW26 <mt.kellybc@gmail.com>
2026-05-13 09:52:56 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path';
 import {defineConfig, loadEnv} from 'vite';
 
 export default defineConfig(({mode}) => {
+  base:'/riverwalk-education/',
   const env = loadEnv(mode, '.', '');
   return {
     plugins: [react(), tailwindcss()],
