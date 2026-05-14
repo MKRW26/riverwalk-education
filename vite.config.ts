9150e796ad0530f1dd9e3072809b6882982434f0
MKRW26 <mt.kellybc@gmail.com>
2026-05-14 11:18:43 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,10 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  // We add the curly braces {} here so 'const' is allowed
-  const env = loadEnv(mode, process.cwd(), '');
-  
-  return {
-    plugins: [react()],
-    // This 'base' line is CRITICAL for GitHub Pages to find your files
-    base: '/riverwalk-education/',
-  }
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+  ],
+  base: "/Riverwalk-Edu-Mockup-1/",
 })
